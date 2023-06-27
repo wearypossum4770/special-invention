64d180230e7020504799bbf583afcb26c9387abf
Dev Steve <wearypossum4770@yahoo.com>
2023-06-27 00:16:02 -0400

    routes

--- checkRoutes.js
+++ checkRoutes.js
@@ -1,11 +1,13 @@
-const { readFile, writeFile } = require("fs");
-
+const { readFile, writeFile, mkdir } = require("fs");
+const { dirname } = require("path");
 const routes = [
   "app/routes/posts.$slug.jsx",
   "app/routes/posts.admin.jsx",
   "app/routes/posts.admin._index.jsx",
   "app/routes/posts.admin.new.jsx",
   "app/routes/posts.admin.$slug.jsx",
+  "app/routes/projects._index.jsx",
+  "app/routes/projects.$id.jsx",
 ];
 
 for (let i = 0; i < routes.length; i++) {
@@ -13,9 +15,16 @@ for (let i = 0; i < routes.length; i++) {
     if (err?.code === "ENOENT") {
       return writeFile(routes[i], "", (err) => {
         if (err)
-          console.log(
-            `Check all parent locations exists for this file ${routes[i]}`
-          );
+          return mkdir(dirname(routes[i]), { recursive: true }, (err) => {
+            if (err) {
+              console.log("could not make parent directories.");
+            } else {
+              console.log(`Creating the parent directories.
+            Ensure it was created correctly.
+            Check all parent locations exists for this file ${routes[i]}
+            `);
+            }
+          });
       });
     }
     return data;
