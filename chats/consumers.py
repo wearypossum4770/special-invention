c439e100bdcde7001e0fc54f46ea00cf63b978d1
Dev Steve <wearypossum4770@yahoo.com>
2023-07-15 19:01:21 -0400

    fix merge conflicts

--- chats/consumers.py
+++ chats/consumers.py
@@ -11,7 +11,17 @@ from chats.views import (get_current_chat, get_last_10_messages,
 User = get_user_model()
 
 
-class ChatConsumer(WebsocketConsumer):
+def command_switch(command):
+    pass
+
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.user = self.scope["user"]
+        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+        self.room_group_name = f"chat_{self.room_name}"
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+        await self.accept()
 
     def fetch_messages(self, data):
         messages = get_last_10_messages(data['chatId'])
@@ -21,52 +31,22 @@ class ChatConsumer(WebsocketConsumer):
         }
         self.send_message(content)
 
-    def new_message(self, data):
-        user_contact = get_user_contact(data['from'])
-        message = Message.objects.create(
-            contact=user_contact,
-            content=data['message'])
-        current_chat = get_current_chat(data['chatId'])
-        current_chat.messages.add(message)
-        current_chat.save()
-        content = {
-            'command': 'new_message',
-            'message': self.message_to_json(message)
-        }
-        return self.send_chat_message(content)
-
-    def messages_to_json(self, messages):
-        result = []
-        for message in messages:
-            result.append(self.message_to_json(message))
-        return result
-
-    def message_to_json(self, message):
-        return {
-            'id': message.id,
-            'author': message.contact.user.username,
-            'content': message.content,
-            'timestamp': str(message.timestamp)
-        }
-
-    commands = {
-        'fetch_messages': fetch_messages,
-        'new_message': new_message
-    }
-
-    def connect(self):
-        self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = 'chat_%s' % self.room_name
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name,
-            self.channel_name
+    async def receive(self, text_data):
+        session = self.scope["session"]
+        print(session)
+        text_data_json = json.loads(text_data)
+        username, message = text_data_json.get("username"), text_data_json.get(
+            "message"
         )
-        self.accept()
 
-    def disconnect(self, close_code):
-        async_to_sync(self.channel_layer.group_discard)(
+        await self.channel_layer.group_send(
             self.room_group_name,
-            self.channel_name
+            {
+                "username": username,
+                "type": "chat_message",
+                "message": message,
+                "is_logged_in": False,
+            },
         )
 
     def receive(self, text_data):
--- users/views.py
+++ users/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-from django.contrib.auth import LoginView
-# Create your views here.
+from django.contrib.auth import get_user_model
 
-# class UserLoginView(LoginView):
+User = get_user_model()
+
+
+def public_home(request):
+    context = {"title": "Home page"}
+    return render(request, 'users/home.html', context)
+# def registration(request):
+#     context = {}
+#     user = User.objects.create_user
