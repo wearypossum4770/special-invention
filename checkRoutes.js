aaede55df0fb8f56615ead4a6b5976c0b6084632
Dev Steve <wearypossum4770@yahoo.com>
2023-07-16 01:18:48 -0400

    changes

--- checkRoutes.js
+++ checkRoutes.js
@@ -8,6 +8,28 @@ const routes = [
   "app/routes/posts.admin.$slug.jsx",
   "app/routes/projects._index.jsx",
   "app/routes/projects.$id.jsx",
+
+// "app/routes/_auth.login.jsx",
+// "app/routes/_auth.logout.jsx",
+// "app/routes/_auth.signup.jsx",
+// "app/routes/_auth.jsx",
+// "app/routes/_public._index.jsx",
+// "app/routes/_public.about-us.jsx",
+// "app/routes/_public.contact.jsx",
+// "app/routes/_public.jsx",
+// "app/routes/dashboard._index.jsx",
+// "app/routes/dashboard.calendar._index.jsx",
+// "app/routes/dashboard.calendar.$day.jsx",
+// "app/routes/dashboard.calendar.jsx",
+// "app/routes/dashboard.projects.$projectId._index.jsx",
+// "app/routes/dashboard.projects.$projectId.collaborators.jsx",
+// "app/routes/dashboard.projects.$projectId.edit.jsx",
+// "app/routes/dashboard.projects.$projectId.settings.jsx",
+// "app/routes/dashboard.projects.$projectId.tasks.$taskId.jsx",
+// "app/routes/dashboard.projects.$projectId.jsx",
+// "app/routes/dashboard.projects.new.jsx",
+// "app/routes/dashboard.projects.jsx",
+// "app/routes/dashboard_.projects.$projectId.print.jsx",
 ];
 
 for (let i = 0; i < routes.length; i++) {
